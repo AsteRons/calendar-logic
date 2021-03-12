affd8ff274868b65e85f7ad25be1ae6a8e51c45f
Jacek <jaceksawko95@gmail.com>
2021-03-12 22:55:11 +0100

    Spring Boot project Generation

---
+++ src/main/java/com/sidus/server/calendarlogic/CalendarLogicApplication.java
@@ -0,0 +1,13 @@
+package com.sidus.server.calendarlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarLogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidus/server/calendarlogic/CalendarLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidus.server.calendarlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
